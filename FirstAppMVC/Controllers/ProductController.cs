841f47b49cbefc18fbbf02c2f5668dc5c5b12660
ramonaramonr <ramonaramonr@gmail.com>
2020-06-26 10:46:01 +0600

    Created method for adding viewed product ids to user cookies

--- FirstAppMVC/Controllers/ProductController.cs
+++ FirstAppMVC/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using FirstAppMVC.Services.Products;
 using FirstAppMVC.Models;
+using Newtonsoft.Json;
 
 namespace FirstAppMVC.Controllers
 {
@@ -64,7 +65,7 @@ namespace FirstAppMVC.Controllers
                 return View("BadRequest");
             }
 
-
+            AddToViewedProducts(id.Value);
             ProductEditModel product = _productService.GetProductEditModel(id.Value);
             return View(product);
         }
@@ -76,5 +77,27 @@ namespace FirstAppMVC.Controllers
             return RedirectToAction("Index");
         }
 
+        private void AddToViewedProducts(int value)
+        {
+            List<string> allIds = new List<string>();
+            string newId = value.ToString();
+            bool hasProducts = HttpContext.Request.Cookies.TryGetValue("ViewedProducts", out string products);
+            if (hasProducts)
+            {
+                List<string> exIds = JsonConvert.DeserializeObject<List<string>>(products);
+                if (exIds.Contains(newId))
+                {
+                    exIds.Remove(newId);
+                }
+                exIds.Add(newId);
+                allIds = exIds;
+            }
+            else
+            {
+                allIds.Add(newId);
+            }
+            string newProducts = JsonConvert.SerializeObject(allIds);
+            HttpContext.Response.Cookies.Append("ViewedProducts", newProducts);
+        }
     }
 }
